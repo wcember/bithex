74a868fcfdc9ab8935989defd17c5bc9f229ab71
bjc <wcember@gmail.com>
2015-01-31 22:21:47 -0500

    setup.py expanded

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from distutils.core import setup
 
 setup(
+    author='William Cember',
+    author_email='wcember@gmail.com',
+    description='Python package to compile hex into Script (bitcoin scripting language)',
+    keywords=['bitcoin', 'compiler'],
     name='bithex',
-    version='0.1',
     packages=['bithex',],
     license='MIT',
     long_description=open('README.txt').read(),
+    url='https://github.com/wcember/bithex',
+    version='0.1',
 )
