3de3f519d9565d5fd5e336aaf1b6082116eeb4a4
bjc <wcember@gmail.com>
2015-02-03 21:48:49 -0500

    removed sphinxcontrib.napoleon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,7 +35,6 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
-    'sphinxcontrib.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
