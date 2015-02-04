01ca662e420fbc7eb4d22a64c59e9ed7e56b036d
bjc <wcember@gmail.com>
2015-02-03 22:15:56 -0500

    added sphinx.ext.napolean to cond.py extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,7 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
