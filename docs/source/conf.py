0c38f790712b73dfb8cbd8daa58a3c898e0fa356
bjc <wcember@gmail.com>
2015-02-03 22:19:37 -0500

    added spinxcontrib.napoleon

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,6 +36,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.napoleon',
+    'sphinxcontrib.napoleon'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
