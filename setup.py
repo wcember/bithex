238b60621c7bab0bd0a0c25061381f38fa10aff8
bjc <wcember@gmail.com>
2015-02-04 22:24:30 -0500

    updated setup.py for new package #

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from distutils.core import setup
 setup(
     author='William Cember',
     author_email='wcember@gmail.com',
-    description='Python package to compile hex into Script (bitcoin scripting language)',
+    description='Python 2 package to compile hex into Script (bitcoin scripting language)',
     keywords=['bitcoin', 'compiler'],
     name='bithex',
     packages=['bithex',],
     license='MIT',
     url='https://github.com/wcember/bithex',
-    version='1.0.1',
+    version='1.0.2',
 )
