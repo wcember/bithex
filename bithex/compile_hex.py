79ec3c6c5033f7e0be0950a81741396f3406cfde
bjc <wcember@gmail.com>
2015-02-07 15:27:56 -0500

    enhanced unit tests and docstring for compile_hex

--- bithex/compile_hex.py
+++ bithex/compile_hex.py
@@ -5,7 +5,7 @@ class InvalidHexError(Exception):
     def __init__(self, value):
         self.value = value
     def __str__(self):
-        return self.value + 'does not compile into valid Script'
+        return self.value + ' does not compile into valid Script'
 
 def compile_hex(hex_string):
     '''Compile a hex string into Script.
@@ -25,6 +25,10 @@ def compile_hex(hex_string):
     Example:
         >>> compile_hex('aa206fe28c0ab6f1b372c1a6a246ae63f74f931e8365e15a089c68d619000000000087')
         'OP_HASH256 6fe28c0ab6f1b372c1a6a246ae63f74f931e8365e15a089c68d6190000000000 OP_EQUAL'
+        >>> compile_hex('76a914a134408afa258a50ed7a1d9817f26b63cc9002cc88ac')
+        'OP_DUP OP_HASH160 a134408afa258a50ed7a1d9817f26b63cc9002cc OP_EQUALVERIFY OP_CHECKSIG'
+        >>> compile_hex('4104b0bd634234abbb1ba1e986e884185c61cf43e001f9137f23c2c409273eb16e6537a576782eba668a7ef8bd3b3cfb1edb7117ab65129b8a2e681f3c1e0908ef7bac')
+        '04b0bd634234abbb1ba1e986e884185c61cf43e001f9137f23c2c409273eb16e6537a576782eba668a7ef8bd3b3cfb1edb7117ab65129b8a2e681f3c1e0908ef7b OP_CHECKSIG'
         >>> compile_hex('aa206fe28c0ab6f1b372c1a6a246ae63f74f931')
         InvalidHexError
     '''
@@ -50,3 +54,30 @@ def compile_hex(hex_string):
         except AssertionError:
             raise InvalidHexError(hex_string)
     return ' '.join(answer_list)
+
+def classify_hex_script(hex_string):
+    '''Classify the transaction type of a hex string.
+
+    Args:
+        hex_string: A string or unicode string that is the hex representation
+            of a bitcoin script
+
+    Returns:
+        str: The transaction type of the hex string
+
+    Raises:
+        TypeError: Raised if the input hex_string isn't a string
+
+    Example:
+        >>> classify_hex_script('76a914a134408afa258a50ed7a1d9817f26b63cc9002cc88ac')
+        'P2PKH'
+    '''
+    if not isinstance(hex_string, basestring):
+        raise TypeError
+    script = compile_hex(hex_string)
+    return classify_transaction(script)
+
+def classify_script(script):
+    if not isinstance(script, basestring):
+        raise TypeError
+    word_list = script.split()
--- bithex/unit_tests.py
+++ bithex/unit_tests.py
@@ -15,6 +15,14 @@ class CompileHexTests(unittest.TestCase):
         self.assertEqual(compile_hex('76a914aafcc27a4a137976e1fbbed6314296b769793f2488ac'),
                 'OP_DUP OP_HASH160 aafcc27a4a137976e1fbbed6314296b769793f24 OP_EQUALVERIFY OP_CHECKSIG')
         self.assertEqual(compile_hex(''), '')
+    def test_hex_to_script_doc_string(self):
+        self.assertEqual(compile_hex('aa206fe28c0ab6f1b372c1a6a246ae63f74f931e8365e15a089c68d619000000000087'),
+                'OP_HASH256 6fe28c0ab6f1b372c1a6a246ae63f74f931e8365e15a089c68d6190000000000 OP_EQUAL')
+        self.assertEqual(compile_hex('76a914a134408afa258a50ed7a1d9817f26b63cc9002cc88ac'),
+                'OP_DUP OP_HASH160 a134408afa258a50ed7a1d9817f26b63cc9002cc OP_EQUALVERIFY OP_CHECKSIG')
+        self.assertEqual(compile_hex('4104b0bd634234abbb1ba1e986e884185c61cf43e001f9137f23c2c409273eb16e6537a576782eba668a7ef8bd3b3cfb1edb7117ab65129b8a2e681f3c1e0908ef7bac'),
+                '04b0bd634234abbb1ba1e986e884185c61cf43e001f9137f23c2c409273eb16e6537a576782eba668a7ef8bd3b3cfb1edb7117ab65129b8a2e681f3c1e0908ef7b OP_CHECKSIG')
+        self.assertRaises(InvalidHexError, compile_hex, 'aa206fe28c0ab6f1b372c1a6a246ae63f74f931')
     def test_hex_to_script_unicode_input(self):
         self.assertEqual(compile_hex(u'aa01bb87'), 'OP_HASH256 bb OP_EQUAL')
     def test_InvalidHexError(self):
@@ -25,5 +33,6 @@ class CompileHexTests(unittest.TestCase):
     def test_TypeError(self):
         self.assertRaises(TypeError, compile_hex, 0)
         self.assertRaises(TypeError, compile_hex, [])
+
 if __name__ == '__main__':
     unittest.main()
