3d670bf647310577a95e886dbd6967ad216e3f3e
bjc <wcember@gmail.com>
2015-03-07 17:31:03 -0500

    additional unit tests

--- bithex/unit_tests.py
+++ bithex/unit_tests.py
@@ -71,6 +71,10 @@ class ClassifyScriptTests(unittest.TestCase):
                 '04b0bd6342 OP_CHECKSIG')
         self.assertRaises(InvalidHexError, compile_hex,
                 'aa206fe28c0ab6f1b372c1a6a246ae63f74f931')
+        self.assertEqual(classify_hex('76a914a134408afa258a50ed7a1d9817f26b63cc9002cc88ac'),
+                'P2PKH')
+        self.assertEqual(classify_hex('aa106fe28c0ab6f1b372c1a6a246ae63f74f87'),
+                'nonstandard transaction')
 
 class ClassifyHexTests(unittest.TestCase):
     def test_classify_hex_doc_string(self):
